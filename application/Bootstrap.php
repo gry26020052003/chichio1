a1afe8a816b5518007fc1075fbedf64f35f62e63
Tian cCheng <gry260200052003@gmail.com>
2012-05-30 18:44:16 -0700

    updates on Boostrap.php

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,14 +3,24 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap{
 	
 	protected function _initDb()
 	{
-		$db = Zend_Db::factory('Pdo_Mysql', array(
-   							'host' => 'localhost',
-                'username' => 'chichio1',
-               'password' => 'dat8tiantianT@',
-                'dbname'   => 'chichio1_advertise' ));
-	//	'username' => 'tian',
-//		'password' => 'nx349r0x',
-//		'dbname'   => 'chichio1_advertise' ));
+		if($_SERVER['SERVER_NAME'] == 'localhost')
+		{
+			$db = Zend_Db::factory('Pdo_Mysql', array(
+   													 'host' => 'localhost',
+           									 'username' => 'tian',
+												  	 'password' => 'nx349r0x',
+											   		 'dbname'   => 'chichio1_advertise' ));
+		}
+		else 
+		{
+			$db = Zend_Db::factory('Pdo_Mysql', array(
+   													 'host' => 'localhost',
+                						 'username' => 'chichio1',
+               							 'password' => 'dat8tiantianT@',
+                						 'dbname'   => 'chichio1_advertise' ));
+		}
+
+
 		Zend_Registry::set('db', $db);
 		Zend_Db_Table_Abstract::setDefaultAdapter($db);
 	}
