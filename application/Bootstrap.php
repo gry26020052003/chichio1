41164a789dad7ccbadfba07b4e2e271167e1aee5
you@example.com <ssss>
2012-05-30 19:31:06 -0600

    asdfsaf

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -23,7 +23,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap{
 	}
 	
 	
-	protected function _initAutoload()
+   protected function _initAutoload()
   {
 		$autoloader = new Zend_Application_Module_Autoloader(array(
     			        'namespace' => 'Default_',
